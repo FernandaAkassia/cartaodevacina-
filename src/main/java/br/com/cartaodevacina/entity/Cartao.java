6dff380e3cce3289dd64fddd2654d1047504f56e
fernanda akassia <fernandaakassiaferreira@outlook.com>
2026-04-17 16:12:39 -0300

    add variável de dtAplicacao e dtRetorno

--- src/main/java/br/com/cartaodevacina/entity/Cartao.java
+++ src/main/java/br/com/cartaodevacina/entity/Cartao.java
@@ -1,5 +1,9 @@
 package br.com.cartaodevacina.entity;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -43,5 +47,13 @@ public class Cartao {
 	@JoinColumn
 	@ManyToOne
 	Profissional profissional;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	@Column
+	LocalDate dataAplicacao;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	@Column
+	LocalDate dataRetorno;
 
 }
